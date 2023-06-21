ce74766bbdf245fb8006fcd587fa140c5384a67e
Divanny <divannyjpm@gmail.com>
2023-06-21 13:36:28 -0400

    Main branch fixed

---
+++ Academix Backend/Data/Administration/AsignaturasRepo.cs
@@ -0,0 +1,52 @@
+﻿using Data.Common;
+using Data.Entities;
+using Models.Administration;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Administration
+{
+    public class AsignaturasRepo : Repository<Asignatura,AsignaturasModel>
+    {
+        
+            public AsignaturasRepo(DbContext dbContext = null) : base
+            (
+                dbContext ?? new AcadmixEntities(),
+                new ObjectsMapper<AsignaturasModel, Asignatura>(u => new Asignatura()
+                {
+                    idAsignatura = u.idAsignatura,
+                    nombreAsignatura = u.NombreAsignatura,
+                    codigoAsignatura = u.CodigoAsignatura,
+                    idArea = u.idArea,
+                    creditos = u.Creditos
+                   
+                }),
+                (DB, filter) => (from u in DB.Set<Asignatura>().Where(filter)
+                                 join a in DB.Set<Area>() on u.idArea equals a.idArea
+                                 select new AsignaturasModel()
+                                 {
+                                     idAsignatura = u.idAsignatura,
+                                     NombreAsignatura = u.nombreAsignatura,
+                                     CodigoAsignatura = u.codigoAsignatura,
+                                     idArea = u.idArea,
+                                     Area = a.nombre,
+                                     Creditos = u.creditos
+                                 })
+            )
+            { }
+        public AsignaturasModel GetByUsername(string nombreAsignatura)
+        {
+            return this.Get(x => x.nombreAsignatura == nombreAsignatura).FirstOrDefault();
+        }
+
+        public AsignaturasModel GetByCode(string codigoAsignatura)
+        {
+            return this.Get(x => x.codigoAsignatura == codigoAsignatura).FirstOrDefault();
+        }
+
+    }
+}
---
+++ Academix Backend/Models/Administration/AreasModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Administration
+{
+    public class AreasModel
+    {
+        public int idArea { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public int idEncargado { get; set; }
+        public string Encargado { get; set; }
+        public int CantCarreras { get; set; }
+        public bool esActivo { get; set; }
+    }
+}
